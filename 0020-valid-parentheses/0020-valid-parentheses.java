74a179f404e5643aca0e35255dafdf31c2afd1b4
Umang Saluja <umangsaluja99@gmail.com>
2026-09-16 21:07:23 +0530

    Time: 5 ms (13.04%), Space: 43.3 MB (57.35%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        HashMap<Character,Character> map = new HashMap<>();
+        map.put('(',')');
+        map.put('[',']');
+        map.put('{','}');
+        Deque<Character> stack = new ArrayDeque<>();
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='[' || c=='{' || c=='('){
+                stack.push(c);
+            }else{
+                if(!stack.isEmpty() && map.get(stack.peek())==c){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
